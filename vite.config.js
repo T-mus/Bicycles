cdcbd4285c2da2c9ff338c86213bce04c123aa05
Raymond Lead <114483647+T-mus@users.noreply.github.com>
2023-11-08 10:52:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig(() => {
     return {
-        base: 'js-vite-template-starter',
+        base: '/Bicycles',
         plugins: [
             Inspect(),
             injectHTML(),
